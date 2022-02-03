574f9274b5683ca672c29412ee2e3e22b312b188
ronihere <74338794+ronihere@users.noreply.github.com>
2022-02-03 10:39:10 +0530

    Add files via upload

---
+++ GRIP_TASK1/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for GRIP_TASK1 project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GRIP_TASK1.settings')
+
+application = get_asgi_application()
